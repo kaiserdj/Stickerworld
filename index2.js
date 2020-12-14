648ff8c8509a352b0629da62ef6833693a2e4d74
Kaiserdj <gonferfr@gmail.com>
2020-12-14 11:33:40 +0100

    whatsapp to class

--- index2.js
+++ index2.js
@@ -1,13 +1,13 @@
 const start = require("./lib/start");
-const whatsapp = require("./lib/whatsapp");
+const { Whatsapp } = require("./lib/whatsapp");
 global.config = require('./config.json');
 const tools = require("./lib/tools");
 
 (async () => {
     try {
         await start.check();
-        const client = await whatsapp.launch();
-        await whatsapp.start(client);
+        const client = await new Whatsapp();
+        await client.start();
     } catch (err) {
         tools.conlog_error(err);
     }
